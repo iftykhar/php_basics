1b3c6e6321b33bba994f5844bf81bf9a6be64215
Iftykhar <s.m.ifty49@gmail.com>
2022-06-26 12:23:05 +0600

    loops sir doing it

---
+++ loops.php
@@ -0,0 +1,17 @@
+<?php
+
+function whileloop($num){
+
+    while($num <= 20){
+        echo "the number is : $num"."<br>";
+        $num--;
+    }
+    return $num;
+}
+
+// whileloop(998);
+
+
+
+
+?>
