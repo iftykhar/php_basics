35a8b8787b6bf73ff8ac5443d0e7b61f6e768b25
Iftykhar <s.m.ifty49@gmail.com>
2022-06-27 20:37:01 +0600

    for loop

--- loops.php
+++ loops.php
@@ -1,5 +1,15 @@
 <?php
 
+// Loops are used to execute the same block of code again and again, as long as a certain condition is true.
+
+// In PHP, we have the following loop types:
+
+// while - loops through a block of code as long as the specified condition is true
+// do...while - loops through a block of code once, and then repeats the loop as long as the specified condition is true
+// for - loops through a block of code a specified number of times
+// foreach - loops through a block of code for each element in an array
+
+#while loop
 function whileloop($num){
 
     while($num <= 20){
@@ -12,6 +22,24 @@ function whileloop($num){
 // whileloop(998);
 
 
+#do...while loog
 
+
+function dowhile($x){
+
+    do{
+        echo "the number :$x"."<br>";
+        $x--;
+    }while($x/2);
+}
+
+dowhile(30);
+
+#for loop 
+// lets try out a factorial function with for loop
+function facto($num){
+    $fact = 1;
+    
+}
 
 ?>
